58a87f8cc45fb6b6299062a2d04834854b17921f
Alvaro Mena <alvamen@gmail.com>
2022-11-24 13:12:48 -0600

    codigo base

---
+++ src/main/java/cr/ac/ucr/paraiso/analisis/demohibernate/DemohibernateApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.paraiso.analisis.demohibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemohibernateApplication.class, args);
+	}
+
+}
